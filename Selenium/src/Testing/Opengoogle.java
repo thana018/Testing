d99a809e61ae42c9b0de97c778d111ab878a279a
Thanalakshmi <Thanalakshmi@ODS-IN-C2CBZ93>
2024-09-21 19:48:38 +0530

    Firstprogram

--- Selenium/src/Testing/Opengoogle.java
+++ Selenium/src/Testing/Opengoogle.java
@@ -1,5 +1,9 @@
 package Testing;
 
 public class Opengoogle {
-
+      
+       public static void main(String[] args) {
+   		// TODO Auto-generated method stub
+           System.out.println("Test");
+   	}
 }
